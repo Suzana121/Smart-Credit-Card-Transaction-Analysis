f493b59a8bebbcf13af65b26bf4d07b11e5e83ae
Lilach Shekter <Lilach.Shekter@gmail.com>
2026-03-15 21:04:59 +0200

    UI: Stats screen - donut chart, insights, responsive layout
    TODO: TransactionRow component needs to be unified across
    Stats, Transactions and SharedInfo screens

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,4 +94,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //Graph libraries for design
+    implementation("com.patrykandpatrick.vico:compose:1.13.1")
+    implementation("com.patrykandpatrick.vico:compose-m3:1.13.1")
+    implementation("com.patrykandpatrick.vico:core:1.13.1")
 }
